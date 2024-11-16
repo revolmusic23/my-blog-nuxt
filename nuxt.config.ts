fc5961ffd67a44aac60aacaa825f863c78f972d8
revolmusic23 <revolmusic23@gmail.com>
2024-11-16 14:44:12 +0800

    feat: 設定基礎 tailwind css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,4 @@
 export default defineNuxtConfig({
-  // 記得先在最上面引入所需的類型
   app: {
     baseURL: "/my-blog-nuxt/",
     buildAssetsDir: "assets",
@@ -7,19 +6,20 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxt/content"],
   css: ["~/assets/css/main.css"],
-  // content 的設定要放在 runtimeConfig.public 裡面
-  runtimeConfig: {
-    public: {
-      content: {
-        highlight: {
-          theme: "github-dark",
-        },
-        markdown: {
-          toc: {
-            depth: 3,
-            searchDepth: 3,
-          },
-        },
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  content: {
+    highlight: {
+      theme: "github-dark",
+    },
+    markdown: {
+      toc: {
+        depth: 3,
+        searchDepth: 3,
       },
     },
   },
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,14 @@
-const animate = require("tailwindcss-animate");
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class"],
   content: [
-    "./pages/**/*.{ts,tsx,vue}",
-    "./components/**/*.{ts,tsx,vue}",
-    "./app/**/*.{ts,tsx,vue}",
-    "./content/**/*.md",
+    "./components/**/*.{js,vue,ts}", // 確保包含了 components 目錄
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+    "./app.vue",
+    "./error.vue",
   ],
   theme: {
     container: {
@@ -19,17 +20,39 @@ module.exports = {
     },
     extend: {
       colors: {
+        primary: {
+          DEFAULT: "var(--primary)",
+          light: "var(--primary-light)",
+          dark: "var(--primary-dark)",
+        },
+        // 如果要用 RGB，可以這樣用
+        // "primary-rgb": {
+        //   DEFAULT: "rgb(var(--primary-rgb))",
+        //   light: "rgb(var(--primary-light-rgb))",
+        //   dark: "rgb(var(--primary-dark-rgb))",
+        // },
+        secondary: "hsl(var(--secondary))",
+
+        text: {
+          primary: "var(--text-primary)", // 主要文字顏色
+          secondary: "var(--text-secondary)", // 次要文字顏色
+          light: "var(--text-light)", // 淺色文字
+          dark: "var(--text-dark)", // 深色文字
+        },
+
+        bg: {
+          primary: "var(--bg-primary)", // 主背景色
+          secondary: "var(--bg-secondary)", // 次要背景色
+          card: "var(--bg-card)", // 卡片背景
+          hover: "var(--bg-hover)", // hover 狀態背景
+        },
+
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
-        primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
-        },
       },
     },
   },
-  plugins: [animate, require("@tailwindcss/typography")],
 };
