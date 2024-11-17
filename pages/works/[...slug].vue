5baa952f5c1768a2da8884e48985a47b32d1f326
revolmusic23 <revolmusic23@gmail.com>
2024-11-17 20:07:02 +0800

    feat: add works page

---
+++ pages/works/[...slug].vue
@@ -0,0 +1,13 @@
+<template>
+  <Card>
+    <ContentDoc v-slot="{ doc }">
+      <article class="prose prose-lg max-w-none">
+        <h1>{{ doc.title }}</h1>
+        <div class="text-gray-500 mb-8">
+          {{ new Date(doc.date).toLocaleDateString() }}
+        </div>
+        <ContentRenderer :value="doc" />
+      </article>
+    </ContentDoc>
+  </Card>
+</template>
---
+++ pages/works/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="gap-12 grid lg:grid-cols-3 md:grid-cols-2">
+    <div v-for="n in 12" :key="n">
+      <Card hover-shadow>
+        <img src="https://via.placeholder.com/300" alt="image" />
+        <div class="mt-4 text-center">
+          <h2 class="text-xl">Title</h2>
+          <p class="text-sm">Description</p>
+        </div>
+      </Card>
+    </div>
+  </div>
+</template>
+
+<script lang="ts"></script>
+
+<style lang=""></style>
