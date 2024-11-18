8c7e35a31044aa99644720cd55cc7e7aad43e406
revolmusic23 <revolmusic23@gmail.com>
2024-11-18 20:35:24 +0800

    feat: add full width layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,17 +3,11 @@
   <div class="min-h-screen bg-bg-secondary">
     <NavBar />
 
-    <!-- 主要內容區 -->
     <div class="container max-w-5xl mx-auto px-4 py-8">
       <div class="flex gap-6">
-        <!-- 側邊欄 -->
-        <SideBar />
-
-        <!-- 主內容區 -->
+        <SideBar v-if="showSidebar" />
         <main class="flex-1">
-          <!-- <div class="bg-bg-primary rounded-lg shadow-md p-4"> -->
           <slot />
-          <!-- </div> -->
         </main>
       </div>
     </div>
@@ -22,4 +16,10 @@
 
 <script setup lang="ts">
 const route = useRoute();
+
+// const showSidebar = computed(() => route.meta.showSidebar);
+const showSidebar = computed(() => {
+  const sidebarRoutes = ["/note", "/article"];
+  return sidebarRoutes.some((path) => route.path.startsWith(path));
+});
 </script>
