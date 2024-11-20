a9fd5f6647e2af4893fb78196448ebdbefed9cbf
revolmusic23 <revolmusic23@gmail.com>
2024-11-20 22:43:57 +0800

    docs: 先新增 footer 檔案

---
+++ components/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+  <footer class="bg-bg-primary border-t">
+    <div class="container mx-auto px-4 max-w-5xl">
+      <div class="h-16"></div>
+    </div>
+  </footer>
+</template>
+<script>
+export default {};
+</script>
+<style lang=""></style>
